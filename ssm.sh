b574c3036ebe96f52d2fc19dab042ab856b47952
sketchsnipe <sketchsnipe@protonmail.com>
2021-08-22 00:04:07 -0400

    lets goo

--- ssm.sh
+++ ssm.sh
@@ -1,57 +1,68 @@
 #!/bin/sh
 
-###############################
-# init [Directories/Varibles] #
-###############################
+# ---- #
+# init #
+# ---- #
 
-# Local varables
-TEMPLATE=$1
-SITE=$2
+for i in "$@"; do
+    case $i in
+        --header=*)
+          HTML_HEAD="${i#*=}"
+          shift # past argument=value
+          ;;
+        --footer=*)
+          HTML_FOOT="${i#*=}"
+          shift # past argument=value
+          ;;
+    esac
+done
 
-# Setup a clean working directory
-rm -rf $SITE
-mkdir $SITE
-
-# Copy files over to working directory
-cp -Lrf $TEMPLATE/* $SITE 
+# Local Varables
+TEMPLATE=$(cd $1; pwd)
+SITE=$(rm -rf $2; mkdir $SITE; cd $2; pwd)
 
 # Global varables
 export BASE=$(cd $SITE; pwd)
 
+# Copy template over to site directory
+cp -Lrf $TEMPLATE/* $SITE 
 
-########################################
-# Unfold the textfiles into html files #
-########################################
+
+# ---------------------- #
+# textfiles -> htmlfiles #
+# ---------------------- #
 
 for file in $(find -L $BASE -name "*.txt"); do
     file_html="${file%.txt}.html"
 
-    # Add header
-    cat "$SITE/config/header.html" >> $file_html
-    # Turn []() into html link
+    # If header exists, add header
+    [ -e $HTML_HEAD ] && cat $HTML_HEAD >> $file_html
+
+    # Copy text over, and turn []() into html link
     cat $file | awk '!_ && match($0, /(.*)\[(.+)\]\((.+)\)(.*)/, u)  { _=1; printf("%s<a href='%s'>%s</a>%s", u[1], u[3], u[2], u[4]) }' >> $file_html
-    # Add footer
-    cat "$SITE/config/footer.html" >> $file_html
+
+    # If footer exists, add footer
+    [ -e $HTML_FOOT ] && cat "$SITE/config/footer.html" >> $file_html
     
     # Remove text file
     rm $file
 done
 
-#############################################################################
-# Run scripts in every folder (used for: rss feeds, cloning user's githubs) #
-#############################################################################
-
-# Pulls out every index.sh file
-BASE_DIR_SH=$(find -L $BASE -name "index.sh")
+# ---------------------------- #
+# Run local scripts [index.sh] #
+# ---------------------------- #
 
 # Visit every git repo in the base direcotry
-for INDEX_SH in $BASE_DIR_SH; do
+for INDEX_SH in $(find -L $BASE -name "index.sh"); do
     # Run index.sh
     source "$INDEX_SH"
 done
-
 
 ###################################
 # Push the working directory live #
 ###################################
 
+cd $SITE
+git add ./
+git commit -m "auto-update"
+git push
